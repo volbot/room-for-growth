46edeba85cc9bfb6e54a8a36aa96986fd01f4ef3
volbot <lucasmcdermitt@icloud.com>
2022-07-24 13:13:32 -0400

    switched movement algorithm to A*

--- src/main.rs
+++ src/main.rs
@@ -28,4 +28,3 @@ async fn main() {
         next_frame().await
     }
 }
-
--- src/person.rs
+++ src/person.rs
@@ -1,5 +1,6 @@
 use crate::entity::Entity;
 use macroquad::prelude::*;
+use pathfinding::prelude::astar;
 
 pub struct Person {
     pub target: Option<(usize, usize)>,
@@ -21,25 +22,19 @@ impl Person {
         }
         let time = get_time();
         if time >= self.last_act + 0.3 {
-            println!("{} {}", self.entity.pos.0, self.entity.pos.1);
-            println!("{} {}", self.target.unwrap().0, self.target.unwrap().1);
-            let xdist: isize = self.entity.pos.0 as isize - self.target.unwrap().0 as isize;
-            let ydist: isize = self.entity.pos.1 as isize - self.target.unwrap().1 as isize;
-            if xdist.abs() >= ydist.abs() && xdist != 0 {
-                let change: isize = if xdist > 0 {-1} else {1};
-                if change.is_positive() {
-                    self.entity.pos.0 += change as usize;
-                } else {
-                    self.entity.pos.0 -= change.abs() as usize;
-                }
-            } else if ydist != 0 {
-                let change: isize = if ydist > 0 {-1} else {1};
-                if change.is_positive() {
-                    self.entity.pos.1 += change as usize;
-                } else {
-                    self.entity.pos.1 -= change.abs() as usize;
-                }
+            let goal: (i32, i32) = (self.target.unwrap().0 as i32, self.target.unwrap().1 as i32);
+            let curr: (i32, i32) = (self.entity.pos.0 as i32, self.entity.pos.1 as i32);
+            if goal == curr {
+                self.target = None;
+                return
             }
+            let result = astar( &(curr),
+                            |&(x, y)| vec! [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+                            .into_iter().map(|p| (p, 1)),
+                            |&(x, y)| (goal.0.abs_diff(x) + goal.1.abs_diff(y)) / 3,
+                            |&p| p == goal);
+            let new_pos = *result.unwrap().0.get(1).unwrap();
+            self.entity.pos = (new_pos.0 as usize, new_pos.1 as usize);
             self.last_act = time;
         }
     }
